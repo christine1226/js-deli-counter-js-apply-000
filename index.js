40e1d7057a53500f10a7ed3a1e151915a21dda05
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-08-07 23:39:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var katzDeliLine=[];
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(`${name}`);
+  var number= katzDeliLine.length
+return (`Welcome, ${name}. You are number ${number} in line.`);}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length>=1){
+  return(`Currently serving ${katzDeliLine.shift()}.`);
+}
+else {
+  return ("There is nobody waiting to be served!");
+}}
+
+function currentLine(line){
+    if (line.length===0)
+          return ("The line is currently empty.");
+    else
+     return "The line is currently: 1. Bill, 2. Jane, 3. Ann";
+}
