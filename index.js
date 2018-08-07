1d3de19d6733cb820d4330c28ce0431443a57e15
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-08-07 23:59:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ function currentLine(line){
     if (line.length===0)
           return ("The line is currently empty.");
     else
-     return "The line is currently: 1. `line[0]` , 2. `line.splice[1]` , 3. `line.splice[2]` ";
+     return "The line is currently: 1. `line[0]` , 2. `line[1]` , 3. `line[2]` ";
 }
