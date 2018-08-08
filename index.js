f902d82dd7c9de65f928ca952671a2fe0e74d105
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-08-08 00:09:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function currentLine(line){
     if (line.length===0)
           return ("The line is currently empty.");
     else
-     return ("The line is currently: 1. +line; ")
+     return ("The line is currently: 1. line[0]; ")
 }
