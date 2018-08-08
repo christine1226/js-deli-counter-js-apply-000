472ec02e45a161d8e7990b95981087c15b6b8a0f
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-08-08 00:10:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ else {
 
 
 function currentLine(line){
-  var line =["Bill", "Jane", "Ann"];
+  var line =[];
     if (line.length===0)
           return ("The line is currently empty.");
     else
